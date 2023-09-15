cc8c8a414516ea5fa5b33e152fdae8e05155b491
Terance Edmonds <teranceedmonds20@gmail.com>
2023-09-16 00:57:36 +0530

    home and events page

---
+++ app/views/includes/side-bars/left.php
@@ -0,0 +1,30 @@
+<section class="side-bar">
+    <div class="inner-section no-border">
+        <div class="input-wrap">
+            <div class="input">
+                <span class="icon material-icons-outlined">
+                    search
+                </span>
+                <input type="text" placeholder="Search">
+            </div>
+        </div>
+    </div>
+
+    <div class="inner-section">
+        <div class="title-wrap">
+            <p class="title">Event Filter</p>
+            <span class="icon material-icons-outlined">
+                tune
+            </span>
+        </div>
+
+        <div class="content filter-checkboxes">
+            <div class="checkbox-wrap">
+                Sports Tournament
+            </div>
+            <div class="checkbox-wrap" data-active="true">
+                Musical
+            </div>
+        </div>
+    </div>
+</section>
