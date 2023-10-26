c4166ce41fda9a362ed216220c96030fa4dc0d2c
Raguram Ketheeswaran <ram.ragu2404@gmail.com>
2023-10-26 23:36:08 +0530

    Sponser UI Finished

--- app/controllers/events.php
+++ app/controllers/events.php
@@ -11,4 +11,21 @@ class Events extends Controller
     {
         $this->view("events/event");
     }
+
+    public function dashboard()
+    {
+        $path = $_GET["url"];
+        $menu_data = [
+            "menu" => [
+                ["id" => "eventDetails", "name" => "Event Details", "icon" => "emoji_events", "path" => "/events/event/dashboard"],
+                ["id" => "sponsers", "name" => "Sponsers", "icon" => "diversity_2", "path" => "/events/event/sponsers"]
+                
+            ]
+        ];
+        $data = [
+            "menu_data" => $menu_data
+        ];
+
+        $this->view($path, $data);
+    }
 }
