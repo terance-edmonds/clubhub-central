94daa7e39a1079f2f93462c0b3b1ccc7b5ab0ba2
Terance Edmonds <teranceedmonds20@gmail.com>
2023-10-26 20:00:21 +0530

    club dashboard table

--- app/views/club/dashboard/index.php
+++ app/views/club/dashboard/index.php
@@ -1,11 +1,11 @@
 <head>
-    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/club-dashboard.css">
     <link rel="stylesheet" href="<?= ROOT ?>/assets/css/side-bar.css">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/club-dashboard.css">
 </head>
 
 <?php $this->view('includes/header') ?>
 
-<div id="club-dashboard" class="container container-sections side-padding">
+<div id="club-dashboard-event" class="container container-sections side-padding club-dashboard dashboard-container">
     <?php $this->view('includes/side-bars/club/dashboard/left', $menu_data)  ?>
 
     <section class="center-section">
@@ -27,6 +27,114 @@
                     </span>
                     <input type="text" placeholder="Search">
                 </div>
+            </div>
+        </div>
+
+        <div class="content-section">
+            <div class="table-wrap">
+                <table>
+                    <tr class="table-header">
+                        <th>Event Name</th>
+                        <th>Date & Time</th>
+                        <th>Venue</th>
+                        <th>View</th>
+                        <th>Status</th>
+                        <th>Action</th>
+                    </tr>
+                    <tr class="table-data">
+                        <td>Freshers' Day</td>
+                        <td>11/04/23 - 10.00 AM</td>
+                        <td>UCSC Ground</td>
+                        <td>
+                            <button class="icon-button">
+                                <span class="material-icons-outlined">
+                                    visibility
+                                </span>
+                            </button>
+                        </td>
+                        <td>
+                            <button class="button status-button" data-status="PENDING">
+                                Pending
+                            </button>
+                        </td>
+                        <td>
+                            <div class="buttons">
+                                <button class="icon-button">
+                                    <span class="material-icons-outlined">
+                                        edit
+                                    </span>
+                                </button>
+                                <button class="icon-button cl-red">
+                                    <span class="material-icons-outlined">
+                                        delete
+                                    </span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="table-data">
+                        <td>Freshers' Day</td>
+                        <td>11/04/23 - 10.00 AM</td>
+                        <td>UCSC Ground</td>
+                        <td>
+                            <button class="icon-button">
+                                <span class="material-icons-outlined">
+                                    visibility
+                                </span>
+                            </button>
+                        </td>
+                        <td>
+                            <button class="button status-button" data-status="APPROVED">
+                                Approved
+                            </button>
+                        </td>
+                        <td>
+                            <div class="buttons">
+                                <button class="icon-button">
+                                    <span class="material-icons-outlined">
+                                        edit
+                                    </span>
+                                </button>
+                                <button class="icon-button cl-red">
+                                    <span class="material-icons-outlined">
+                                        delete
+                                    </span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr class="table-data">
+                        <td>Freshers' Day</td>
+                        <td>11/04/23 - 10.00 AM</td>
+                        <td>UCSC Ground</td>
+                        <td>
+                            <button class="icon-button">
+                                <span class="material-icons-outlined">
+                                    visibility
+                                </span>
+                            </button>
+                        </td>
+                        <td>
+                            <button class="button status-button" data-status="REJECTED">
+                                Rejected
+                            </button>
+                        </td>
+                        <td>
+                            <div class="buttons">
+                                <button class="icon-button">
+                                    <span class="material-icons-outlined">
+                                        edit
+                                    </span>
+                                </button>
+                                <button class="icon-button cl-red">
+                                    <span class="material-icons-outlined">
+                                        delete
+                                    </span>
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
             </div>
         </div>
     </section>
