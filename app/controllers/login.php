2e87f31c7b53d4828f14df3ff30c9ece699a27a1
Terance Edmonds <teranceedmonds20@gmail.com>
2024-04-23 15:59:18 +0530

    user email validation

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -9,6 +9,8 @@ class Login extends Controller
         ];
 
         if ($_SERVER['REQUEST_METHOD'] == "POST") {
+            $form_data = $_POST;
+
             $db = new Database();
             $user = new User($db);
             $user_invitation = new UserInvitation($db);
@@ -20,48 +22,50 @@ class Login extends Controller
 
             try {
                 /* validate the form data */
-                // if($user->validateLogin($data)) {}
+                if ($user->validateLogin($form_data)) {
+                    $result = $user->one(['email' => $_POST['email']]);
 
-                $result = $user->one(['email' => $_POST['email']]);
+                    if ($result) {
+                        if (!$result->is_verified) {
+                            $data['errors']['email'] = "User account not verified";
+                        } else if ($result->is_blacklisted) {
+                            $data['errors']['email'] = "User account has been suspended";
+                        } else if (password_verify($_POST['password'], $result->password)) {
+                            $params = $_GET;
+                            $success = true;
 
-                if ($result) {
-                    if (!$result->is_verified) {
-                        $data['errors']['email'] = "User account not verified";
-                    } else if ($result->is_blacklisted) {
-                        $data['errors']['email'] = "User account has been suspended";
-                    } else if (password_verify($_POST['password'], $result->password)) {
-                        $params = $_GET;
-                        $success = true;
+                            if (!empty($params) && !empty($params['token'])) {
+                                $invitation_result = $user_invitation->one(["invitation_code" => $params['token'], "is_valid" => 1]);
 
-                        if (!empty($params) && !empty($params['token'])) {
-                            $invitation_result = $user_invitation->one(["invitation_code" => $params['token'], "is_valid" => 1]);
+                                if (!empty($invitation_result) && $invitation_result->user_id == $result->id) {
+                                    $user_invitation->update(["id" => $invitation_result->id], ["is_valid" => 0]);
 
-                            if (!empty($invitation_result) && $invitation_result->user_id == $result->id) {
-                                $user_invitation->update(["id" => $invitation_result->id], ["is_valid" => 0]);
+                                    /* create club member record */
+                                    if ($invitation_result->club_role && $invitation_result->club_id) {
+                                        $club_member_data = [
+                                            "user_id" => $invitation_result->user_id,
+                                            "club_id" => $invitation_result->club_id,
+                                            "role" => $invitation_result->club_role,
+                                            "state" => "ACCEPTED"
+                                        ];
 
-                                /* create club member record */
-                                if ($invitation_result->club_role && $invitation_result->club_id) {
-                                    $club_member_data = [
-                                        "user_id" => $invitation_result->user_id,
-                                        "club_id" => $invitation_result->club_id,
-                                        "role" => $invitation_result->club_role,
-                                        "state" => "ACCEPTED"
-                                    ];
-
-                                    $club->update(["id" => $invitation_result->club_id], ["state" => "ACTIVE"]);
-                                    $club_member->create($club_member_data);
+                                        $club->update(["id" => $invitation_result->club_id], ["state" => "ACTIVE"]);
+                                        $club_member->create($club_member_data);
+                                    }
+                                } else {
+                                    $success = false;
+                                    $_SESSION['alerts'] = [["status" => "error", "message" => "Failed to login, invalid token or token has expired."]];
                                 }
-                            } else {
-                                $success = false;
-                                $_SESSION['alerts'] = [["status" => "error", "message" => "Failed to login, invalid token or token has expired."]];
                             }
+                        } else {
+                            $data['errors']['password'] = "Incorrect password";
                         }
                     } else {
-                        $data['errors']['password'] = "Incorrect password";
+                        $data['errors']['email'] = "User not found";
                     }
-                } else {
-                    $data['errors']['email'] = "User not found";
                 }
+
+                $data['errors'] = $user->errors;
 
                 $db->commit();
 
